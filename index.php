6021c0c28c9abb8f044d085f48e638d3272d17b0
ddany <duminydaniel@gmail.com>
2017-09-11 23:55:17 +0530

    Update index.php

--- index.php
+++ index.php
@@ -14,9 +14,9 @@
     <body>
 	    <div class="button-open">Let's Chat</div>
 
-               <div class="box">
-                <div class="button-close">^</div>
-           </div>
+               
+                
+           
         
 	    <div class="headerBar">
                 <div class="user-photo"><img src="https://i.pinimg.com/736x/f1/86/78/f18678b7fc471157b01330df6b072f11--korean-artist-pencil-portrait.jpg"></div>
@@ -26,7 +26,7 @@
         
         
         <div class="chatbox"> 
-		
+	  <div class="button-close">^</div>
             <div class="chatlogs">
             
                 <div class="chat friend">
