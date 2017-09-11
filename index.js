0e4200facc4872d543df697d32a4792c3dc18093
ddany <duminydaniel@gmail.com>
2017-09-11 23:45:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -67,7 +67,25 @@ $('document').ready(function(){
 
 	});
 
+$(function () {
+  $(".button-open").hide();
+  $(".button-close").bind("click", function () {
+    $(".box").hide(100);        
 
+    if ($(this).attr("class") == "button-close")
+    {
+      $(".button-open").show();
+    }
+  });
+});
+
+$(".button-open").bind("click", function () {
+    $(".box").show(100);        
+    if ($(this).attr("class") == "button-open")
+    {
+      $(".button-open").hide();
+    }
+  });
 
 
 	//----------------------User Sends Message Methods--------------------------------//
